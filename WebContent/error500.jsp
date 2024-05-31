b5b64ac9282e215528ef1779f3260d89c77a706b
saldm04 <s.dimartino25@studenti.unisa.it>
2024-05-31 20:44:32 +0200

    id 5 - resolved

---
+++ WebContent/error500.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Errore Interno del Server</title>
+</head>
+<body>
+    <h1>Errore 500 - Errore Interno del Server</h1>
+    <p>Si è verificato un errore imprevisto. Stiamo lavorando per risolverlo il prima possibile.</p>
+    <p>Per favore, torna alla <a href="<%= request.getContextPath() %>/Home.jsp">homepage</a> e riprova più tardi.</p>
+</body>
+</html>
--- src/it/unisa/control/AccountServlet.java
+++ src/it/unisa/control/AccountServlet.java
@@ -22,112 +22,85 @@ import it.unisa.model.UserDao;
  */
 @WebServlet("/account")
 public class AccountServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
-	}
+    private static final long serialVersionUID = 1L;
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		String redirectedPage = request.getParameter("page");
-		
-		UserDao daoUser = new UserDao();
-		UserBean user = (UserBean) request.getSession().getAttribute("currentSessionUser");
-		IndirizzoSpedizioneBean sped = new IndirizzoSpedizioneBean();
-		IndirizzoSpedizioneDao daoSped = new IndirizzoSpedizioneDao();
-		MetodoPagamentoBean pag = new MetodoPagamentoBean();
-		MetodoPagamentoDao daoPag = new MetodoPagamentoDao();
-		String action = request.getParameter("action");
-		
-		String nome = request.getParameter("nome");
-		String cognome = request.getParameter("cognome");
-		String telefono = request.getParameter("tel");
-		String citta = request.getParameter("citta");
-		String ind = request.getParameter("ind");
-		String cap = request.getParameter("cap");
-		String prov = request.getParameter("prov");	
-		
-		String tit = request.getParameter("tit");
-		String numC = request.getParameter("numC");
-		String scad = request.getParameter("scad");
-		
-			
-		try {
-			if(action!=null) {
-				if(action.equalsIgnoreCase("addS")) {
-					
-					if(daoSped.doRetrieveByKey(ind, cap)==null) {
-						
-						 sped.setNome(nome);
-						 sped.setCognome(cognome);
-						 sped.setIndirizzo(ind);
-						 sped.setTelefono(telefono);
-						 sped.setCap(cap);
-						 sped.setProvincia(prov);
-						 sped.setCitta(citta);
-						 daoSped.doSave(sped);
-						 
-					}
-					
-					daoUser.doUpdateSpedizione(user.getEmail(), ind, cap);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
 
-				}
-				
-				else if(action.equalsIgnoreCase("removeS")) {
-					daoUser.doUpdateSpedizione(user.getEmail(), null, null);
-					request.getSession().removeAttribute("spedizione");
-					
-				}
-				
-				else if(action.equalsIgnoreCase("addP")) {
-					if(daoPag.doRetrieveByKey(numC)==null) {
-						pag.setTitolare(tit);
-						pag.setNumero(numC);
-						pag.setScadenza(scad);
-						daoPag.doSave(pag);
-					
-				}
-					
-					daoUser.doUpdatePagamento(user.getEmail(),numC);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String redirectedPage = request.getParameter("page");
 
-				}
-				else if(action.equalsIgnoreCase("removeP")) {
-					daoUser.doUpdatePagamento(user.getEmail(),null);
-					request.getSession().removeAttribute("pagamento");
+        UserDao daoUser = new UserDao();
+        UserBean user = (UserBean) request.getSession().getAttribute("currentSessionUser");
+        IndirizzoSpedizioneBean sped = new IndirizzoSpedizioneBean();
+        IndirizzoSpedizioneDao daoSped = new IndirizzoSpedizioneDao();
+        MetodoPagamentoBean pag = new MetodoPagamentoBean();
+        MetodoPagamentoDao daoPag = new MetodoPagamentoDao();
+        String action = request.getParameter("action");
 
-				}
-			}
-		}catch(SQLException e) {
-			e.printStackTrace();		}
-			
-		if(user.getIndirizzo()!=null && user.getCap()!=null) {
-			try {
-				request.getSession().removeAttribute("spedizione");
-				request.getSession().setAttribute("spedizione", daoSped.doRetrieveByKey(user.getIndirizzo(),user.getCap()));
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-		if(user.getCartaDiCredito()!=null) {
-			try {
-				request.getSession().setAttribute("pagamento", daoPag.doRetrieveByKey(user.getCartaDiCredito()));
-			} catch (NumberFormatException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/" + redirectedPage);
-		dispatcher.forward(request, response);
+        String nome = request.getParameter("nome");
+        String cognome = request.getParameter("cognome");
+        String telefono = request.getParameter("tel");
+        String citta = request.getParameter("citta");
+        String ind = request.getParameter("ind");
+        String cap = request.getParameter("cap");
+        String prov = request.getParameter("prov");    
 
-	}
+        String tit = request.getParameter("tit");
+        String numC = request.getParameter("numC");
+        String scad = request.getParameter("scad");
 
+        try {
+            if(action != null) {
+                if(action.equalsIgnoreCase("addS")) {
+                    if(daoSped.doRetrieveByKey(ind, cap) == null) {
+                        sped.setNome(nome);
+                        sped.setCognome(cognome);
+                        sped.setIndirizzo(ind);
+                        sped.setTelefono(telefono);
+                        sped.setCap(cap);
+                        sped.setProvincia(prov);
+                        sped.setCitta(citta);
+                        daoSped.doSave(sped);
+                    }
+                    daoUser.doUpdateSpedizione(user.getEmail(), ind, cap);
+                } else if(action.equalsIgnoreCase("removeS")) {
+                    daoUser.doUpdateSpedizione(user.getEmail(), null, null);
+                    request.getSession().removeAttribute("spedizione");
+                } else if(action.equalsIgnoreCase("addP")) {
+                    if(daoPag.doRetrieveByKey(numC) == null) {
+                        pag.setTitolare(tit);
+                        pag.setNumero(numC);
+                        pag.setScadenza(scad);
+                        daoPag.doSave(pag);
+                    }
+                    daoUser.doUpdatePagamento(user.getEmail(), numC);
+                } else if(action.equalsIgnoreCase("removeP")) {
+                    daoUser.doUpdatePagamento(user.getEmail(), null);
+                    request.getSession().removeAttribute("pagamento");
+                }
+            }
+
+            if(user.getIndirizzo() != null && user.getCap() != null) {
+                request.getSession().removeAttribute("spedizione");
+                request.getSession().setAttribute("spedizione", daoSped.doRetrieveByKey(user.getIndirizzo(), user.getCap()));
+            }
+
+            if(user.getCartaDiCredito() != null) {
+                request.getSession().setAttribute("pagamento", daoPag.doRetrieveByKey(user.getCartaDiCredito()));
+            }
+
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/" + redirectedPage);
+            dispatcher.forward(request, response);
+
+        } catch (SQLException e) {
+            log("SQL Error: " + e.getMessage(), e);
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        } catch (Exception e) {
+            log("Unexpected Error: " + e.getMessage(), e);
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
+
